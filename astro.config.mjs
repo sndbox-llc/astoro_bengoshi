c48340460c1e5c14b4fd364458fde83909d46343
sandbox co.,ltd <info@sndbox.jp>
2026-03-25 11:37:10 +0900

    d

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,17 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 
-// https://astro.build/config
+const SITE_URL = 'https://hotaka-law.hotaka-g.jp/'
+
 export default defineConfig({
-  site: 'https://hotaka-law.hotaka-g.jp/',
+  site: SITE_URL,
   integrations: [sitemap()],
-  // 💡 ViteのCSSプリプロセッサ設定を追加
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
           // node_modules（依存関係）内の古いSass書き方による警告を非表示にする
           quietDeps: true,
-          // 最新のSassコンパイラ APIを使用することを明示（推奨）
-          // api: 'modern-compiler',
         },
       },
     },
